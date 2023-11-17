c39bc3b21ffb2bfbc714391ebedf2ed7abba772c
laeladiyahpuspita <117339456+laeladiyahpuspita@users.noreply.github.com>
2023-11-17 16:19:34 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+document.getElementById('profession').innerText = 'UI/UX';
+document.getElementById('name').innerText = 'Laela Diyah Puspita';
+document.getElementById('passion').innerText = 'mengembangkan aplikasi web';
+document.getElementById('skills').innerText = 'HTML, CSS, JavaScript, PYTHON';
+document.getElementById('experience').innerText = 'Pengalaman pelatihan UI/UX, Python Fundamental, Cyber Security ';
+document.getElementById('hobbies').innerText = 'Swimming, Desain, Mathematics';
+document.getElementById('goal').innerText = 'Menjadi seorang UI/UX dan Analisis Data terbaik';
+document.getElementById('email').innerText = 'Laeladiyahpuspita280203@gmail.com';
